927a07615df1ed3bd624fc0f62c7686f0e3c3df6
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-07-30 23:37:42 +0530

    🟡 Add 0133. Clone Graph [Medium] (Java)

---
+++ Medium/0133-Clone-Graph/solution.java
@@ -0,0 +1,36 @@
+// ═══════════════════════════════════════════════════════
+//  Problem  : 0133. Clone Graph
+//  URL      : https://leetcode.com/problems/clone-graph/
+//  Difficulty : Medium
+//  Language : Java
+//  Runtime  : 7 ms
+//  Memory   : 42.6 MB
+//  Solved   : July 30, 2026
+// ═══════════════════════════════════════════════════════
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        return node;
+        
+    }
+}
