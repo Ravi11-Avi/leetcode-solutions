6a4efc80ad0602ea85a3afca0ab3180a816f54d7
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-07-30 23:28:53 +0530

    🟡 Add 0096. Unique Binary Search Trees [Medium] (Java)

--- Medium/0096-Unique-Binary-Search-Trees/solution.java
+++ Medium/0096-Unique-Binary-Search-Trees/solution.java
@@ -10,12 +10,19 @@
 
 class Solution {
     public int numTrees(int n) {
+        if (n <=2 )return n; 
+    int dp[]= new int[n];
     if (n<1) return n;
     int ans= 0;
-    for (int i = 0 ; i < n ; i++){
-        ans += numTrees(n-1)* numTrees(i-1);
+
+ 
+    dp[1]=1; 
+    dp[0]=1;
+    for (int i = 2; i <= n ; i++){
+            ans += dp[n-1]*  dp[i-1];
 
     }
-    return ans;
+    return ans ;
     }
+   
 }
