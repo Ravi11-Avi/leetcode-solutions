f56ad1a427bfda6dcc142be5514b05dafabd46ae
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-08-09 00:18:57 +0530

    🟡 Add 0662. Maximum Width of Binary Tree [Medium] (Java)

--- Medium/0662-Maximum-Width-of-Binary-Tree/solution.java
+++ Medium/0662-Maximum-Width-of-Binary-Tree/solution.java
@@ -1,10 +1,10 @@
 // ═══════════════════════════════════════════════════════
 //  Problem  : 0662. Maximum Width of Binary Tree
-//  URL      : https://leetcode.com/problems/maximum-width-of-binary-tree/
+//  URL      : https://leetcode.com/problems/maximum-width-of-binary-tree/submissions/2099514696/
 //  Difficulty : Medium
 //  Language : Java
-//  Runtime  : 0 ms
-//  Memory   : 42.7 MB
+//  Runtime  : 1 ms
+//  Memory   : 45.2 MB
 //  Solved   : August 9, 2026
 // ═══════════════════════════════════════════════════════
 
@@ -59,7 +59,7 @@ class Solution {
 
 
                 if (currP.node.left!= null)queue.offer(new Pair(node.left, 2 * currIndex + 1));
-                if (currP.node.right!= null)queue.offer(new Pair(node.right, 2 * currIndex + 1));
+                if (currP.node.right!= null)queue.offer(new Pair(node.right, 2 * currIndex + 2));
             }
             maxWidth = Math.max(maxWidth, last-first+1);
         }
