722f4b0f0bdd61649e23b1ad150401bf910b2cca
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-08-17 18:34:03 +0530

    🔴 Add 0127. Word Ladder [Hard] (Java)

--- Hard/0127-Word-Ladder/solution.java
+++ Hard/0127-Word-Ladder/solution.java
@@ -4,43 +4,58 @@
 //  Difficulty : Hard
 //  Language : Java
 //  Runtime  : 0 ms
-//  Memory   : 42.7 MB
+//  Memory   : 42.5 MB
 //  Solved   : August 17, 2026
 // ═══════════════════════════════════════════════════════
 
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        
-        Set<String> set = new  HashSet<>(wordList);
-        Queue<String> queue =  new LinkedList<>();
 
-        queue.offer(beginWord);
-
-        int wordcount  = 1 ; 
-
-        while (!queue.isEmpty()){
-            String curr =  queue.poll();
-
-            if (curr == endWord ) return wordcount;
-            
-            char[] wordchar =  curr.toCharArray();
-
-            for (int i = 0 ; i < wordchar.length ; i++){
-                char ch = wordchar[i];
-
-                for (char c = 'a'; c<='z';c++){
-                    wordchar[i] = c;
-                    String newword =  new String(wordchar);
-
-                    if(set.contains(newword)){
-                        queue.offer(newword);
-                        set.remove(newword);
-                    }
-                }
-            }
+        Set<String> set = new HashSet<>(wordList);
+        Queue<String> queue = new LinkedList<>();
+        if (!set.contains(endWord)) {
+            return 0;
         }
 
-        return  0 ;
+        queue.offer(beginWord);
+        set.remove(beginWord);
 
-    }
-}
+
+        int wordcount = 1;
+
+        while (!queue.isEmpty()) {
+            int size =  queue.size();
+
+            for(int j = 0 ; j <size; j++){
+                String curr = queue.poll();
+
+                if (curr.equals(endWord))
+                    return wordcount;
+
+                char[] wordchar = curr.toCharArray();
+
+                for (int i = 0; i < wordchar.length; i++) {
+                    char ch = wordchar[i];
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        wordchar[i] = c;
+                        String newword = new String(wordchar);
+
+                        if (set.contains(newword)) {
+                            queue.offer(newword);
+                            set.remove(newword);
+                        }
+                    }
+                wordchar[i] = ch;
+                }
+                
+
+            }
+              wordcount++;  
+        
+
+        }
+
+    return 0;
+
+}}
