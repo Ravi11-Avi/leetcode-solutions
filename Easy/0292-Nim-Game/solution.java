0ae0450405cf6176a85107198b7bfd38dfcf5251
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-08-28 21:00:57 +0530

    🟢 Add 0292. Nim Game [Easy] (Java)

--- Easy/0292-Nim-Game/solution.java
+++ Easy/0292-Nim-Game/solution.java
@@ -4,16 +4,17 @@
 //  Difficulty : Easy
 //  Language : Java
 //  Runtime  : 0 ms
-//  Memory   : 42.1 MB
+//  Memory   : 42.4 MB
 //  Solved   : August 28, 2026
 // ═══════════════════════════════════════════════════════
 
 class Solution {
     public boolean canWinNim(int n) {
-        if (n%3 ==0 &&(n/3)%2 == 0){
-            return true;
-        }else{
-            return false;
-        }
+        // if (n%3 <3 &&(n/3)%2 == 0){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+        return n% 4 !=0;
     }
 }
