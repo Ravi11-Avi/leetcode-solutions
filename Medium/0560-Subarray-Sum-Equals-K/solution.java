ea2564f2506eabc29c2bd5349261596cc02ed93f
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-08-31 11:56:28 +0530

    🟡 Add 0560. Subarray Sum Equals K [Medium] (Java)

--- Medium/0560-Subarray-Sum-Equals-K/solution.java
+++ Medium/0560-Subarray-Sum-Equals-K/solution.java
@@ -1,10 +1,10 @@
 // ═══════════════════════════════════════════════════════
 //  Problem  : 0560. Subarray Sum Equals K
-//  URL      : https://leetcode.com/problems/subarray-sum-equals-k/
+//  URL      : https://leetcode.com/problems/subarray-sum-equals-k/submissions/2125775033/
 //  Difficulty : Medium
 //  Language : Java
-//  Runtime  : 0 ms
-//  Memory   : 42.2 MB
+//  Runtime  : 23 ms
+//  Memory   : 49 MB
 //  Solved   : August 31, 2026
 // ═══════════════════════════════════════════════════════
 
@@ -22,7 +22,7 @@ class Solution {
             if (map.containsKey(currsume-k)){
                     count +=map.get(currsume-k);
             }
-            map.put(n , map.getOrDefault(n,0)+1);
+            map.put(currsume , map.getOrDefault(currsume,0)+1);
         }
 
         return count;
