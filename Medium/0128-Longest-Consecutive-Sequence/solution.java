56d3ca501b61ed20e53df3054e971f58b8d1b4e8
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-02 15:38:17 +0530

    🟡 Add 0128. Longest Consecutive Sequence [Medium] (Java)

--- Medium/0128-Longest-Consecutive-Sequence/solution.java
+++ Medium/0128-Longest-Consecutive-Sequence/solution.java
@@ -10,15 +10,18 @@
 
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        int curcount = 0;
-        int max =  0;
 
-        for(int i = 0 ; i < nums.length ; i++){
-            if (nums[i]==nums[i]-1){
+        if (nums.length <1)return 0;
+        Arrays.sort(nums);
+        int curcount = 1;
+        int max =  1;
+
+        for(int i = 0 ; i < nums.length-1 ; i++){
+            if(nums[i]==nums[i+1])continue;
+            if (nums[i]==nums[i+1]-1){
                 curcount++;
             }else{
-                curcount=0;
+                curcount=1;
             }
             if (max< curcount)max=curcount;
         }
