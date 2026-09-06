9a8dae52e1b88d332aecb65d9a87b932f4106515
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-06 19:05:28 +0530

    🟡 Add 0380. Insert Delete GetRandom O(1) [Medium] (Java)

---
+++ Medium/0380-Insert-Delete-GetRandom-O1/solution.java
@@ -0,0 +1,57 @@
+// ═══════════════════════════════════════════════════════
+//  Problem  : 0380. Insert Delete GetRandom O(1)
+//  URL      : https://leetcode.com/problems/insert-delete-getrandom-o1/submissions/2132891040/
+//  Difficulty : Medium
+//  Language : Java
+//  Runtime  : 30 ms
+//  Memory   : 99.5 MB
+//  Solved   : September 6, 2026
+// ═══════════════════════════════════════════════════════
+
+class RandomizedSet {
+
+    ArrayList<Integer> arr ;
+    HashMap<Integer, Integer> map ;
+
+    public RandomizedSet() {
+        map =  new HashMap<>();
+        arr=  new ArrayList<>();
+        
+    }
+    
+    public boolean insert(int val) {
+        if(map.containsKey(val)) return false;
+        map.put(val, arr.size());
+        arr.add(val);
+        return true;
+        
+    }
+    
+    public boolean remove(int val) {
+        if(!map.containsKey(val))return false;
+        int idx =  map.get(val);
+
+        Collections.swap(arr,idx,arr.size()-1);
+        int sw  = arr.get(idx);
+        map.put(sw,idx);
+        arr.remove(arr.size()-1);
+        map.remove(val);
+        return true ;
+        
+    }
+    
+    public int getRandom() {
+        Random rm = new Random();
+        int m = rm.nextInt(arr.size());
+        return arr.get(m);
+        
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
