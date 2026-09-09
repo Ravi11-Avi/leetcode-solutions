d40ffd5abfb7968014d4fdef4a2b165e3079e9b3
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-09 19:04:44 +0530

    🟡 Add 0011. Container With Most Water [Medium] (Java)

---
+++ Medium/0011-Container-With-Most-Water/solution.java
@@ -0,0 +1,30 @@
+// ═══════════════════════════════════════════════════════
+//  Problem  : 0011. Container With Most Water
+//  URL      : https://leetcode.com/problems/container-with-most-water/
+//  Difficulty : Medium
+//  Language : Java
+//  Runtime  : 0 ms
+//  Memory   : 42.5 MB
+//  Solved   : September 9, 2026
+// ═══════════════════════════════════════════════════════
+
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = 0 ; 
+        int max= 0 ;
+
+        while (i< j){
+            int wid = i-1;
+
+            int cm =  Math.min(height[i], height[j])* wid;
+
+            if (max< cm) max= cm;
+
+            if (height[i]< height[j]) i++;
+            else j--;
+       }
+
+       return max;
+    }
+}
