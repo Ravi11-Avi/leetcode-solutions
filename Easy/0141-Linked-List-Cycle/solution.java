8239f1470f49b52bdb52e5a8d9fafff2bcdce925
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-11 10:28:08 +0530

    🟢 Add 0141. Linked List Cycle [Easy] (Java)

--- Easy/0141-Linked-List-Cycle/solution.java
+++ Easy/0141-Linked-List-Cycle/solution.java
@@ -27,7 +27,7 @@ public class Solution {
         ListNode first  =  head ;
         ListNode second  =  head ;
 
-        while (first.next !=null || first!= null || second !=  null || second.next != null){
+        while ( first!= null || second !=  null){
             first = first.next.next;
             second = second.next;
 
