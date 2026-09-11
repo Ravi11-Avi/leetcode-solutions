148c4ad64424b16ac265f39aeb4feae88d66d914
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-11 10:46:39 +0530

    🟢 Add 0202. Happy Number [Easy] (Java)

---
+++ Easy/0202-Happy-Number/solution.java
@@ -0,0 +1,36 @@
+// ═══════════════════════════════════════════════════════
+//  Problem  : 0202. Happy Number
+//  URL      : https://leetcode.com/problems/happy-number/
+//  Difficulty : Easy
+//  Language : Java
+//  Runtime  : 0 ms
+//  Memory   : 41.8 MB
+//  Solved   : September 11, 2026
+// ═══════════════════════════════════════════════════════
+
+class Solution {
+    public boolean isHappy(int n) {
+
+        HashSet<Integer> set  = new HashSet<>();
+
+        while(n!= 1 && !set.contains(n)){
+            set.add(n);
+            n = newNumber(n);
+
+        }
+
+        return n==1;
+    }
+
+    public int newNumber(int n ){
+        int number = 0;
+         while (n>0){
+            int digit =  n%10 ;
+            number =number*10 +digit;
+            n /=10;
+         }
+
+
+         return number ;
+    }
+}
