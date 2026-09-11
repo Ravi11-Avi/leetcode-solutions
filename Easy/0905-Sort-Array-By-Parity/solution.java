eafb725cba1e3a4ae7a1711147c5dc84c7aad2ee
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-11 19:22:11 +0530

    🟢 Add 0905. Sort Array By Parity [Easy] (Java)

---
+++ Easy/0905-Sort-Array-By-Parity/solution.java
@@ -0,0 +1,33 @@
+// ═══════════════════════════════════════════════════════
+//  Problem  : 0905. Sort Array By Parity
+//  URL      : https://leetcode.com/problems/sort-array-by-parity/
+//  Difficulty : Easy
+//  Language : Java
+//  Runtime  : 0 ms
+//  Memory   : 42.9 MB
+//  Solved   : September 11, 2026
+// ═══════════════════════════════════════════════════════
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int l = 0;
+        int r = nums.length -1;
+        while (l< r){
+            if (nums[l]%2 !=0){
+
+                if (nums[l]%2 ==0){
+                    int temp  =  nums[l];
+                    nums[l++]=  nums[r];
+                    nums[r--]= temp ;
+
+                }
+                r--;
+            }
+            l++;
+        }
+
+        return nums;
+
+        
+    }
+}
