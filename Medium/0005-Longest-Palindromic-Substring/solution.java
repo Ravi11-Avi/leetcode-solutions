91c2c97daf9b579e88ffd50394f77f90696c8c7f
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-12 14:53:41 +0530

    🟡 Add 0005. Longest Palindromic Substring [Medium] (Java)

--- Medium/0005-Longest-Palindromic-Substring/solution.java
+++ Medium/0005-Longest-Palindromic-Substring/solution.java
@@ -4,7 +4,7 @@
 //  Difficulty : Medium
 //  Language : Java
 //  Runtime  : 0 ms
-//  Memory   : 42.8 MB
+//  Memory   : 42.7 MB
 //  Solved   : September 12, 2026
 // ═══════════════════════════════════════════════════════
 
@@ -13,7 +13,7 @@ class Solution {
 
         
 
-        String p  =  new String();
+        String p = s.substring(0, 1);
         if (s == null || s.length() <= 1) {
             return s; 
         }        
@@ -23,17 +23,25 @@ class Solution {
             int r  = i+1 ;
 
             while (l>=0 && r<s.length()&& s.charAt(l)== s.charAt(r)){
-                 p = s.substring(l,r+1);
-                l--;
-                r++;
+
+                if ((r-l+1)> p.length()){
+                         p = s.substring(l,r+1);
+                  
+                }
+                  l--;
+                    r++;
+                
             }
             l=i;
             r= i+1;
             while (l>=0 && r<s.length()&& s.charAt(l)== s.charAt(r)){
                 
-                 p = s.substring(l,r+1);
-                l--;
-                r++;
+               if ((r-l+1)> p.length()){
+                         p = s.substring(l,r+1);
+               
+                }
+                  l--;
+                    r++;
             }
             
             
