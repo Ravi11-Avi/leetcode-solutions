063deb618cd446f2c38a261bb3c5418762363df8
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-14 23:23:25 +0530

    🟡 Add 0713. Subarray Product Less Than K [Medium] (Java)

--- Medium/0713-Subarray-Product-Less-Than-K/solution.java
+++ Medium/0713-Subarray-Product-Less-Than-K/solution.java
@@ -1,21 +1,24 @@
 // ═══════════════════════════════════════════════════════
 //  Problem  : 0713. Subarray Product Less Than K
-//  URL      : https://leetcode.com/problems/subarray-product-less-than-k/
+//  URL      : https://leetcode.com/problems/subarray-product-less-than-k/submissions/2141862976/
 //  Difficulty : Medium
 //  Language : Java
-//  Runtime  : 0 ms
-//  Memory   : 43 MB
+//  Runtime  : 3 ms
+//  Memory   : 48.6 MB
 //  Solved   : September 14, 2026
 // ═══════════════════════════════════════════════════════
 
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
         int count = 0 ,product =1 ,left = 0 ; 
 
         for (int right  = 0 ; right < nums.length ; right ++){
             product *=nums[right];
 
-            if (product >= k && left < right){
+            while (product >= k && left <= right){
                 product /= nums[left];
                 left++;
             }
