1a38e3caa373cb68d9aee9fe2b1b10741ad032f4
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-17 17:12:56 +0530

    🔴 Add 0239. Sliding Window Maximum [Hard] (Java)

---
+++ Hard/0239-Sliding-Window-Maximum/solution.java
@@ -0,0 +1,35 @@
+// ═══════════════════════════════════════════════════════
+//  Problem  : 0239. Sliding Window Maximum
+//  URL      : https://leetcode.com/problems/sliding-window-maximum/
+//  Difficulty : Hard
+//  Language : Java
+//  Runtime  : 0 ms
+//  Memory   : 42.8 MB
+//  Solved   : September 17, 2026
+// ═══════════════════════════════════════════════════════
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        
+
+        int l = 0 ;
+        int r = k;
+
+        int[] result =  new int[nums.length +1];
+        int idx= 0 ;
+
+        while(r < nums.length){
+
+            int max =  0 ; 
+            for(int i = l ; i< r+1 ; i++)max = Math.max(max,nums[i]);
+
+            result[idx++]= max; 
+            l++;
+            r++;
+            
+        
+
+        }
+        return result;
+    }
+}
