02dc951b75c0800487e0ac5fcf2ac78b4efe71bb
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-20 23:29:08 +0530

    🟢 Add 3498. Reverse Degree of a String [Easy] (Java)

---
+++ Easy/3498-Reverse-Degree-of-a-String/solution.java
@@ -0,0 +1,25 @@
+// ═══════════════════════════════════════════════════════
+//  Problem  : 3498. Reverse Degree of a String
+//  URL      : https://leetcode.com/problems/reverse-degree-of-a-string/?envType=daily-question&envId=2026-09-20
+//  Difficulty : Easy
+//  Language : Java
+//  Runtime  : 0 ms
+//  Memory   : 41.8 MB
+//  Solved   : September 20, 2026
+// ═══════════════════════════════════════════════════════
+
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        
+
+        for (char c : s.toCharArray()){
+            if (c >= 'A' && c <= 'Z') {
+                sum += ('Z' - c + 1);
+            }
+
+
+        }
+        return sum;
+    }
+}
