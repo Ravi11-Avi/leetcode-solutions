bb0d142d8a88b0d18176452e4d82f3d0de86cf32
Ravi Ranjan Bhakt <raviranjan.bhakt@gmail.com>
2026-09-25 14:23:16 +0530

    🟢 Add 0020. Valid Parentheses [Easy] (Java)

--- Easy/0020-Valid-Parentheses/solution.java
+++ Easy/0020-Valid-Parentheses/solution.java
@@ -4,18 +4,31 @@
 //  Difficulty : Easy
 //  Language : Java
 //  Runtime  : 0 ms
-//  Memory   : 42.7 MB
+//  Memory   : 42.2 MB
 //  Solved   : September 25, 2026
 // ═══════════════════════════════════════════════════════
 
 class Solution {
     public boolean isValid(String s) {
-        boolean close =  true;
-        for (char  c : s.toCharArray()){
-            if (c == '('||c == '{' || c=='[')close = false;
-            else close = true;
-        }
+        Stack<Character>stack = new Stack<>();
+        for (char c :  s.toCharArray()){
+            if (c == '{'|| c=='(' || c=='[')stack.push(c);
+            else if (c== '}'){
+                if (!stack.isEmpty() && stack.peek()!= '{')return false;
+                stack.pop();
 
-        return close;
+            }else if (c== ']'){
+                if (!stack.isEmpty() &&stack.peek()!= '[')return false;
+                stack.pop();
+
+            }else if (c== ')'){
+                if (!stack.isEmpty() && stack.peek()!= '(')return false;
+                stack.pop();
+
+            }
+
+           
+        }
+         return stack.isEmpty();
     }
 }
